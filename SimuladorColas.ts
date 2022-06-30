d8b080fcb5ccb91aec74a5b5d4f5f0f379eba843
agustinlimon <agustinlimonta@gmail.com>
2022-06-30 18:44:37 -0300

    1 file modified, 2 added

---
+++ SimuladorColas.ts
@@ -0,0 +1,10 @@
+import { Empleado } from "./Empleado";
+import { EstadoCliente } from "./EstadoCliente";
+import { Evento } from "./Evento";
+import { Cliente } from "./Cliente";
+import { Simulador } from "./Simulador";
+import { Utils } from "./Utils";
+
+export class SimuladorColas extends Simulador { 
+  
+}
